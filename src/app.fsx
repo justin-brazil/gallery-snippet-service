2d812c5bfacc4d0e759b458d8efede39b5bd97e4
Tomas Petricek <tomas@tomasp.net>
2017-01-27 16:31:11 +0000

    Add support for multiple sources

--- src/app.fsx
+++ src/app.fsx
@@ -31,7 +31,10 @@ type Snippet =
     author : string
     twitter : string
     code : string 
-    compiled : string }
+    compiled : string 
+    version : string
+    config : string
+    hidden : bool }
 
 type NewSnippet = 
   { title : string
@@ -39,7 +42,10 @@ type NewSnippet =
     author : string
     twitter : string
     compiled : string
-    code : string }
+    code : string 
+    hidden : bool
+    config : string
+    version : string }
 
 // --------------------------------------------------------------------------------------
 // Reading & writing blobs in Azure storage
@@ -67,77 +73,87 @@ let fromJson<'R> str : 'R =
   use tr = new System.IO.StringReader(str)
   serializer.Deserialize(tr, typeof<'R>) :?> 'R
 
-let readSnippets () =
-  let container = createCloudBlobClient().GetContainerReference("olympics")
+let readSnippets source =
+  let container = createCloudBlobClient().GetContainerReference(source)
   if container.Exists() then
     let blob = container.GetBlockBlobReference("snippets.json")
     if blob.Exists() then 
       let json = blob.DownloadText(System.Text.Encoding.UTF8) 
-      json, json |> fromJson<Snippet[]> |> List.ofArray
+      json, json |> fromJson<Snippet[]> 
     else failwith "Blob 'snippets.json' does not exist."
-  else failwith "container 'olympics' not found" 
+  else failwithf "Container '%s' not found" source
 
-let writeSnippets (json:string) = 
-  let container = createCloudBlobClient().GetContainerReference("olympics")
+let writeSnippets source (snippets:Snippet[]) = 
+  let json = snippets |> toJson
+  let container = createCloudBlobClient().GetContainerReference(source)
   if container.Exists() then
     let blob = container.GetBlockBlobReference("snippets.json")
     blob.UploadText(json, System.Text.Encoding.UTF8)
-  else failwith "container 'olympics' not found" 
+  else failwithf "container '%s' not found" source
 
 // --------------------------------------------------------------------------------------
 // Keeping current snippets using an agent
 // --------------------------------------------------------------------------------------
 
 type Message = 
-  | GetSnippets of AsyncReplyChannel<string>
-  | AddSnippet of NewSnippet * AsyncReplyChannel<int>
-  | LikeSnippet of int
+  | GetSnippets of string * AsyncReplyChannel<string>
+  | AddSnippet of string * NewSnippet * AsyncReplyChannel<int>
+  | LikeSnippet of string * int
 
 let snippets = MailboxProcessor.Start(fun inbox -> 
-  let rec loop (json, snippets) = async {
+  let rec loop snippets = async {
     let! msg = inbox.Receive()
     match msg with
-    | GetSnippets(res) ->
-        res.Reply(json)
-        return! loop (json, snippets)
-    | AddSnippet(snip, res) ->
-        let id = 1 + (snippets |> Seq.map (fun s -> s.id) |> Seq.fold max 0)
-        let snippets = 
-          { id = id; likes = 0; posted = DateTime.Now; author = snip.author; 
-            title = snip.title; compiled = snip.compiled; code = snip.code; 
-            description = snip.description; twitter = snip.twitter }::snippets
-        let json = snippets |> Array.ofList |> toJson
+    | GetSnippets(source, res) ->
+        match Map.tryFind source snippets with
+        | Some(json, _) -> 
+            res.Reply(json)
+            return! loop snippets
+        | None ->
+            let json, snips = readSnippets source
+            res.Reply(json)
+            return! loop (Map.add source (json, snips) snippets)
+    | AddSnippet(source, snip, res) ->
+        let _, snips = snippets.[source]
+        let id = 1 + (snips |> Seq.map (fun s -> s.id) |> Seq.fold max 0)
+        let snippet = 
+          { id = id; likes = 0; posted = DateTime.Now; author = snip.author
+            version = snip.version; hidden = snip.hidden; title = snip.title 
+            compiled = snip.compiled; code = snip.code; config = snip.config 
+            description = snip.description; twitter = snip.twitter }
+        let snips = Array.append snips [| snippet |]
+        writeSnippets source snips
         res.Reply(id)
-        writeSnippets json
-        return! loop (json, snippets)
-    | LikeSnippet(id) ->
-        let snippets = snippets |> List.map (fun s -> if s.id = id then { s with likes = s.likes + 1 } else s)
-        let json = snippets |> Array.ofList |> toJson
-        writeSnippets json
-        return! loop (json, snippets) }
+        return! loop (Map.add source (toJson snips, snips) snippets)
+    | LikeSnippet(source, id) ->
+        let snips = snippets.[source] |> snd |> Array.map (fun s -> 
+            if s.id = id then { s with likes = s.likes + 1 } else s)
+        writeSnippets source snips
+        return! loop (Map.add source (toJson snips, snips) snippets)  }
   async { 
-    try return! loop (readSnippets())
-    with e -> printfn "Agent failed: %A" e })
+    while true do
+      try return! loop Map.empty
+      with e -> printfn "Agent failed: %A" e })
 
 // --------------------------------------------------------------------------------------
 // REST API for Snippets
 // --------------------------------------------------------------------------------------
 
 let likeSnippet = 
-  GET >=> pathScan "/olympics/%d/like" (fun id ->
-    snippets.Post(LikeSnippet id)
+  GET >=> pathScan "/%s/%d/like" (fun (source, id) ->
+    snippets.Post(LikeSnippet (source, id))
     Successful.OK "Liked")
 
 let postSnippet = 
-  POST >=> path "/olympics" >=> request (fun req ctx -> async {
-    let snip = Text.Encoding.UTF8.GetString(req.rawForm) |> fromJson<NewSnippet>
-    let! id = snippets.PostAndAsyncReply(fun ch -> AddSnippet(snip, ch))
+  POST >=> pathScan "/%s" (fun source ctx -> async {
+    let snip = Text.Encoding.UTF8.GetString(ctx.request.rawForm) |> fromJson<NewSnippet>
+    let! id = snippets.PostAndAsyncReply(fun ch -> AddSnippet(source, snip, ch))
     return! Successful.CREATED (string id) ctx })
 
 let getSnippets = 
-  GET >=> path "/olympics" >=> fun ctx -> async {
-    let! json = snippets.PostAndAsyncReply(GetSnippets)
-    return! Successful.OK json ctx }
+  GET >=> pathScan "/%s" (fun source ctx -> async {
+    let! json = snippets.PostAndAsyncReply(fun ch -> GetSnippets(source, ch))
+    return! Successful.OK json ctx })
 
 let app =
   setHeader  "Access-Control-Allow-Origin" "*"
